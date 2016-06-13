c80b68d3330270fc07b9b2f516b6e0e24fe60fd7
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-13 00:57:31 +0000

    Adding config file

---
+++ config/sitemap.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'cache' => [
+        'enabled'  => false,
+
+        'key'      => 'laravel-sitemap.your-key',
+
+        'lifetime' => 3600,
+    ],
+
+    'escaping'        => true,
+
+    'use-limit-size'  => false,
+
+    'max-size'        => null,
+
+    'styles' => [
+        'enabled'  => true,
+
+        'location' => null,
+    ],
+];
