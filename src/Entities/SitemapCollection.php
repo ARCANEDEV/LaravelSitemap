cfa1680f46eb013253ae545494c57b858e111bf5
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-19 01:22:16 +0000

    Adding SitemapCollection Entity

---
+++ src/Entities/SitemapCollection.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanedev\LaravelSitemap\Entities;
+
+use Arcanedev\Support\Collection;
+
+/**
+ * Class     SitemapCollection
+ *
+ * @package  Arcanedev\LaravelSitemap\Entities
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class SitemapCollection extends Collection
+{
+    //
+}
