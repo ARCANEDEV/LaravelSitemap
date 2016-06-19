2dca684d1fd63376d4d6ebdd75107247f1a3cbc4
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-19 01:18:15 +0000

    Adding the SitemapGenerator

---
+++ src/Contracts/SitemapGenerator.php
@@ -0,0 +1,63 @@
+<?php namespace Arcanedev\LaravelSitemap\Contracts;
+
+/**
+ * Interface  SitemapGenerator
+ *
+ * @package   Arcanedev\LaravelSitemap\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface SitemapGenerator
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the sitemap styles location.
+     *
+     * @return string
+     */
+    public function getStylesLocation();
+
+    /**
+     * Set the sitemap styles location.
+     *
+     * @param  string  $location
+     *
+     * @return self
+     */
+    public function setStylesLocation($location);
+
+    /**
+     * Set the use styles value.
+     *
+     * @param  bool  $useStyles
+     *
+     * @return self
+     */
+    public function setUseStyles($useStyles);
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @param  array        $data
+     * @param  string       $format
+     * @param  string|null  $style
+     *
+     * @return array
+     */
+    public function generate(array $data, $format = 'xml', $style = null);
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check is styles is enabled.
+     *
+     * @return bool
+     */
+    public function isStylesEnabled();
+}
---
+++ src/Helpers/SitemapGenerator.php
@@ -0,0 +1,171 @@
+<?php namespace Arcanedev\LaravelSitemap\Helpers;
+
+use Arcanedev\LaravelSitemap\Contracts\SitemapGenerator as SitemapGeneratorContract;
+use Arcanedev\LaravelSitemap\Contracts\SitemapStyler as SitemapStylerContract;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\View\Factory as View;
+use Illuminate\Support\Arr;
+
+/**
+ * Class     SitemapGenerator
+ *
+ * @package  Arcanedev\LaravelSitemap\Helpers
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class SitemapGenerator implements SitemapGeneratorContract
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @var  \Illuminate\Contracts\View\Factory */
+    protected $view;
+
+    /** @var  \Illuminate\Contracts\Cache\Repository */
+    protected $cache;
+
+    /** @var  \Arcanedev\LaravelSitemap\Contracts\SitemapStyler */
+    protected $styler;
+
+    /** @var  array */
+    protected $contentTypes = [
+        'html'         => 'text/html',
+        'ror-rdf'      => 'text/rdf+xml; charset=utf-8',
+        'ror-rss'      => 'text/rss+xml; charset=utf-8',
+        'txt'          => 'text/plain',
+        'sitemapindex' => 'text/xml; charset=utf-8',
+        'xml'          => 'text/xml; charset=utf-8'
+    ];
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * SitemapGenerator constructor.
+     *
+     * @param  \Arcanedev\LaravelSitemap\Contracts\SitemapStyler  $styler
+     * @param  \Illuminate\Contracts\Cache\Repository             $cache
+     * @param  \Illuminate\Contracts\View\Factory                 $view
+     */
+    public function __construct(
+        SitemapStylerContract $styler,
+        Cache $cache,
+        View $view
+    ) {
+        $this->styler = $styler;
+        $this->cache  = $cache;
+        $this->view   = $view;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the sitemap styles location.
+     *
+     * @return string
+     */
+    public function getStylesLocation()
+    {
+        return $this->styler->getLocation();
+    }
+
+    /**
+     * Set the sitemap styles location.
+     *
+     * @param  string  $location
+     *
+     * @return self
+     */
+    public function setStylesLocation($location)
+    {
+        $this->styler->setLocation($location);
+
+        return $this;
+    }
+
+    /**
+     * Set the use styles value.
+     *
+     * @param  bool $useStyles
+     *
+     * @return self
+     */
+    public function setUseStyles($useStyles)
+    {
+        $this->styler->setEnabled($useStyles);
+
+        return $this;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @param  array        $data
+     * @param  string       $format
+     * @param  string|null  $style
+     *
+     * @return array
+     */
+    public function generate(array $data, $format = 'xml', $style = null)
+    {
+        // check if styles are enabled
+        $data['style'] = $this->styler->get($format, $style);
+
+        $content = $this->getContent($data, $format);
+        $headers = $this->getHeaders($format);
+
+        return compact('content', 'headers');
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check is styles is enabled.
+     *
+     * @return bool
+     */
+    public function isStylesEnabled()
+    {
+        return $this->styler->isEnabled();
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the rendered content.
+     *
+     * @param  array   $data
+     * @param  string  $format
+     *
+     * @return string
+     */
+    private function getContent(array $data, $format)
+    {
+        $content = $this->view->make("sitemap::$format", $data)->render();
+
+        return $content;
+    }
+
+    /**
+     * Get the headers.
+     *
+     * @param  string  $format
+     *
+     * @return array
+     */
+    protected function getHeaders($format)
+    {
+        return [
+            'Content-type' => Arr::get($this->contentTypes, $format, 'text/xml; charset=utf-8')
+        ];
+    }
+}
